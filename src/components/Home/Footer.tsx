8a4945f5dcfd23d0b82d5ca86406aa00cc32414e
Arham <arhamn205@gmail.com>
2023-03-17 21:04:42 +0500

    recreated footer

--- src/components/Home/Footer.tsx
+++ src/components/Home/Footer.tsx
@@ -7,53 +7,78 @@ const Footer = () => {
     const date = new Date();
     return date.getFullYear();
   };
+  const footerPStyle1= "ml-14 hover:color:slate-300 my-2 origin-left transition-all duration-300 hover:scale-105"
+  const footerPStyle2= "ml-7 hover:color:slate-300 my-2 origin-left transition-all duration-300 hover:scale-105"
   return (
     <>
       <div className="relative flex h-vh-80 w-full bg-black">
-        <section className=" w-1/2 h-5/6 flex flex-col items-center">
-          <div className="flex">
-            <Logo height={80} width={80}
-              mr={8} mt={8} ml={0} mb={0} textMarginTop={12}
-            textSize = {'6xl'}
+        <section className="w-1/2 h-5/6 flex flex-col items-center">
+          <div className="flex mt-10 mr-20">
+            <Logo
+              height={80}
+              width={80}
+              mr={8}
+              mt={1}
+              ml={0}
+              mb={0}
+              textMarginTop={0}
+              textSize={"6xl"}
             />
           </div>
-          <p className="text-white ml-36 text-sm">
+          <p className="text-white ml-10 text-sm">
             ULTIMATE SHOPPING DESTINATION
           </p>
-          <button className="h-1/5 w-1/2 mt-16  rounded-lg font-semibold transition-all duration-300 hover:bg-slate-200 bg-white ">
-            Shop Now!
-          </button>
-          {/* make it scroll on top */}
-          <button className="text-white absolute bottom-20 opacity-70 text-lg "
+
+          <div className="mt-6 h-full w-2/3 text-center text-white">
+            {/* add email */}
+            <p>
+              Contact us with any questions or feedback and our customer care
+              team will be happy to assist you
+            </p>
+            <p className="mt-10 text-xl ">&#9993; vibekart@gmail.com</p>
+            <p className="my-3 text-xl">&#9742; +92-123-4567891</p>
+          </div>
+
+          <button
+            className="text-white absolute bottom-20 opacity-70 text-lg "
             onClick={() => {
-          
               window.scrollTo({
                 top: 0,
-                behavior:"smooth"
-              })
+                behavior: "smooth",
+              });
             }}
           >
-            
             Scroll to top
           </button>
         </section>
 
-        <section className="mt-7  flex flex-col justify-center h-5/6 w-1/2 text-white p-12  text-4xl font-bold">
-          <h1 className=" border-l-8 ml-10 border-slate-400 pl-3 ">OVERVIEW</h1>
-          <ul className="flex flex-col w-1/2 text-center  justify-center h-full ">
-            <li className="my-4 hover:scale-105 transition-all duration-300">
-              <button> WHO ARE WE </button>
-            </li>
-            <li className="my-4 hover:scale-105 transition-all duration-300">
-              <button> CONTACT </button>
-            </li>
-            <li className="my-4 hover:scale-105 transition-all duration-300">
-              OUR POLICIES
-            </li>
-            <li className="my-4 hover:scale-105 transition-all duration-300">
-              
-            </li>
-          </ul>
+        <section className="mt-7 flex flex-col h-5/6 w-1/2 text-white p-12  text-4xl font-bold">
+          <div className="w-full h-full flex">
+            <div className="w-full h-full mx-2">
+              <h1 className=" border-l-8 ml-10 border-slate-400 pl-3">
+                VibeKart
+              </h1>
+              <div className=" text-xl font-normal mt-4 ">
+                <p className={`${footerPStyle1}`}>Who are we</p>
+                <p className={`${footerPStyle1}`}>Contact</p>
+                <p className={`${footerPStyle1}`}>Our impact</p>
+                <p className={`${footerPStyle1}`}>Terms & conditions</p>
+                <p className={`${footerPStyle1}`}>Privacy policy</p>
+                <p className={`${footerPStyle1}`}>How to shop</p>
+              </div>
+            </div>
+            <div className="w-full h-full mt-1">
+              <h1 className="border-l-8 ml-3 border-slate-400 pl-3">
+                Work with us
+              </h1>
+              <div className=" text-xl font-normal mt-5">
+                <p className={`${footerPStyle2}`}>Sell with us</p>
+                <p className={`${footerPStyle2}`}>How to get started</p>
+                <p className={`${footerPStyle2}`}>Partner testimonal</p>
+                <p className={`${footerPStyle2}`}>Our Partner support</p>
+              </div>
+            </div>
+          </div>
         </section>
 
         <footer className=" absolute bottom-5 text-3xl left-1/3 text-white">
